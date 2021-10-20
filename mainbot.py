b8ff34227f8d72106aa19f6a1ffb55982e173acc
diyor200 <abdulaxatovdiyorbek40@gmail.com>
2021-10-20 12:20:29 +0500

    Initial Commit

--- mainbot.py
+++ mainbot.py
@@ -1,15 +1,16 @@
 import requests
 import datetime
-from aiogram import Bot,types,Dispatcher,executor
-from fortoken import tg_bot_token,weather_token
+from aiogram import Bot, types, Dispatcher, executor
+from fortoken import tg_bot_token, weather_token
 
 bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=["start"])
 
+@dp.message_handler(commands=["start"])
 async def start_command(message: types.Message):
     await message.reply("Asaalomu alaykum! Iltimos menga shahar nomini yuboring(faqat shahar nomi)")
+
 
 @dp.message_handler()
 async def get_wether(message: types.Message):
@@ -28,12 +29,14 @@ async def get_wether(message: types.Message):
         length_of_the_day = sunset_timestamp - sunrise_timestamp
 
         await message.reply(f"***{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}***\n"
-              f"shahar: {city}\nTemperatura: {cur_weather} C° \n"
-              f"Namlik: {humidity}\nbosim: {pressure} mm.sm.us\nShamol: {wind} m/s\n"
-              f"quyosh chiqishi: {sunrise_timestamp}\nQuyosh botishi: {sunset_timestamp}\nKun davomiyligi: {length_of_the_day}\nKuningiz hayrli o'tsin")
+                            f"shahar: {city}\nTemperatura: {cur_weather} C° \n"
+                            f"Namlik: {humidity}\nbosim: {pressure} mm.sm.us\nShamol: {wind} m/s\n"
+                            f"quyosh chiqishi: {sunrise_timestamp}\nQuyosh botishi: {sunset_timestamp}\nKun davomiyligi: {length_of_the_day}\nKuningiz hayrli o'tsin")
 
 
     except:
         await message.reply("shahar nomi xato kiritildi! 🤦‍♂️")
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp,skip_updates=True)
+    executor.start_polling(dp, skip_updates=True)
